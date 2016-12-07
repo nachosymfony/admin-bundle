0f2628e231a9e1bfe552414b0c842f5f184aa2d4
Yoan Arnaudov <jonidev@gmail.com>
2016-12-08 00:04:00 +0200

    move slug ajax here

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,11 +3,20 @@
 namespace nacholibre\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use nacholibre\AdminBundle\Utils\Slugger;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('nacholibreAdminBundle::index.html.twig');
+class DefaultController extends Controller {
+    /**
+    * @Route("/generate_slug/{query}", name="nacholibre.admin_bundle.generate_slug", options={"expose": true})
+    */
+    public function generateSlugAction($query) {
+        $slugger = new Slugger;
+        $slug = $slugger->slugify($query);
+
+        return new JsonResponse([
+            'slug' => $slug
+        ]);
     }
 }
